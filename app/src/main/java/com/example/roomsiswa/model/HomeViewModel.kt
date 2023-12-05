f0e51f0521000928b9eb43bc01753cf0e0ddc68d
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-12-05 16:32:17 +0700

    Menambahkan Import pada HomeViewModel.kt

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -2,9 +2,13 @@ package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     companion object{
