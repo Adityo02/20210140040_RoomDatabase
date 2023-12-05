adb3407e91cb6738ced7ece87d83bd10280dc9ee
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-12-05 17:20:49 +0700

    Menambahkan SiswaApp() dalam MainActivity.kt

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -16,6 +16,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.theme.halaman.DestinasiEntry
+import com.example.roomsiswa.ui.theme.halaman.DestinasiHome
+import com.example.roomsiswa.ui.theme.halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.theme.halaman.HomeScreen
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController()){
