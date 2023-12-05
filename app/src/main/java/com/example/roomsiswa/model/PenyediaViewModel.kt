21e7b710dcad1f8253482433f20859c7ebdb5815
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-12-05 16:43:58 +0700

    Membuat initializer pada PenyediaViewModel.kt

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 
 object PenyediaViewModel {
-    val Factory = viewModelFactory {}
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
 }
