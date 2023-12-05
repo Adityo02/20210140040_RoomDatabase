be217b93340bef40e10807d1dc2ce6ef7f35424e
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-12-05 16:59:40 +0700

    Membuat object DestinasiEntry pada HalamanHome.kt.kt

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,2 +1,6 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
